75f71c2e7a7a23f0772cdc1130a759b21ea5b3ff
Brian D. Caruso <bdc34@cornell.edu>
2021-02-24 14:50:25 -0500

    Stores the locks in memory, adds refresh, adds unlock on disconnect

--- modapi/collab_app.py
+++ modapi/collab_app.py
@@ -6,14 +6,19 @@ To listen to locks and unlocks subscribe to lock and unlock.
 
 To  lock:
 
-emit lock {id: 1234, user: bob}
+emit lock {id: 1234, username: bob}
 
 To unlock:
 
-emit unlock {id: 1234, user: bob}
+emit unlock {id: 1234}
 """
+from asyncio import Semaphore, gather
+from typing import Dict, List, NamedTuple
+from collections import defaultdict
 
 import socketio
+
+import modapi.lockstore as lockstore
 from .config import allow_origins, debug
 
     
@@ -25,27 +30,95 @@ sio = socketio.AsyncServer(
 )
 
 
+def _validate_id(data):
+    err = []
+    if not data:
+        err.append( 'no data')
+    if 'id' not in data:
+        err.append( 'lacks id')
+    if 'id' in data and not isinstance(data['id'], int):
+            err.append( 'id is not an int')
+    
+    return err
+
+def _validate_id_and_user(data):
+    err = _validate_id(data)
+    if 'username' not in data:
+        err.append( 'lacks user')
+    if 'username' in data and not isinstance(data['username'], str):
+        err.append('user is not a str')
+    return err
+
 @sio.event
-async def lock(sid, *args, **kwargs):
-    # TODO validate data
-    # TODO save lock
-    await sio.emit("lock", args)
+async def lock(sid, data):
+    err = _validate_id_and_user(data)
+    if err:
+        emsg = ", ".join(err)
+        await sio.emit('errors', f'Error on lock, {emsg}', to=sid)
+        sio.logger.error(f'Error with lock from {sid}: {emsg}')
+        return
+
+    user = data['username']
+    id = data['id']
+
+    err = await lockstore.lock(id, sid, user)
+    if err:
+        await sio.emit('errors', f"Error while locking: {err} ", to=sid)
+        return
+    else:        
+        await sio.emit("lock", {'id':id, 'username':user} )
 
 
 @sio.event
-async def unlock(sid, *args, **kwargs):
-    # TODO validate data
-    # TODO Remove lock from store
-    await sio.emit("unlock", args)
+async def unlock(sid, data):
+    sio.logger.error( data )
+    err = _validate_id(data)
+    if err:
+        emsg = ", ".join(err)
+        await sio.emit('errors', f'Error on unlock, {emsg}', to=sid)
+        sio.logger.error(f'Error with unlock from {sid}: {emsg}')
+        return
+
+    id = data['id']
+
+    err = await lockstore.unlock(id, sid, None)
+    if err:
+        await sio.emit('errors', f"Error while unlocking: {err}", to=sid)
+        return
+    else:
+        await _unlock(id)
+
+@sio.event
+async def refresh(sid, data):
+    """Useful for a client to request current locks"""
+    current_locks = await lockstore.current_locks()
+
+    sio.logger.error(current_locks)
+    for id, lck in current_locks.items():
+        await sio.emit("lock", {'id':id,'username':lck.username}, to=sid)
 
 
 @sio.event
 async def connect(sid, env):
     sio.logger.error(f"{__file__} CONNECTED {sid}")
-    sio.logger.error(f"{__file__} ENV IS: {env}")
+    #sio.logger.error(f"{__file__} ENV IS: {env}")
 
 
 @sio.event
 async def disconnect(sid):
-    # TODO unlock all held by this sid
-    print("disconnect ", sid)
+    to_unlock = await lockstore.unlock_for_sid(sid)
+    await _send_unlocks( to_unlock )
+    
+    sio.logger.error(f'disconnect {sid} and unlocked {len(to_unlock)}')
+
+
+async def _send_unlocks(to_unlock: List[int]):
+    """Send a list of unlocks in parallel"""
+    tasks = [_unlock(id) for id in to_unlock] 
+    await gather( *tasks )
+
+
+async def _unlock(id: int):
+    """Emit unlock a single paper id"""
+    await sio.emit("unlock", {'id':id})
+
---
+++ modapi/lockstore.py
@@ -0,0 +1,63 @@
+"""Storage of soft locks on submissions
+"""
+from asyncio import Semaphore
+from typing import Dict, NamedTuple
+
+
+
+class Lock(NamedTuple):
+    username: str
+    sid: int
+
+
+_locks: Dict[int, Lock] = {}
+"""A dict of locked submission ids.
+
+Must aquire semaphore before using this"""
+
+_semaphore = Semaphore()
+"""Semaphore for both locks and users"""
+
+
+# async def is_locked(id):
+#     pass
+
+
+async def lock(sub_id, sid, username):
+    async with _semaphore:
+        if sub_id in _locks:
+            return f"Id {sub_id} already locked"
+        else:
+            _locks[sub_id] = Lock(username=username, sid=sid)
+            return None
+
+
+async def unlock(sub_id, sid, username):
+    async with _semaphore:
+        if sub_id in _locks:
+            return f"Id {sub_id} already locked"
+        elif sid != _locks[sub_id].sid and username != _locks[sub_id].username:
+            return f"Id is not locked by either {sid} or {username}"
+        else:
+            del _locks[sub_id]
+            return None
+
+
+async def unlock_for_sid(sid):
+    """Unlock all locks for a sid.
+
+    Returns list of submission ids unlocked."""
+    async with _semaphore:
+        deleted = []
+        for sub_id, lok in list(_locks.items()):
+            if lok.sid == sid:
+                deleted.append(sub_id)
+                del _locks[sub_id]
+
+        return deleted
+
+
+async def current_locks():
+    async with _semaphore:
+        return _locks.copy()
+ 
