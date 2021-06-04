c6ef2e3a80271a35fe2072bba378988803e50a14
Brian D. Caruso <bdc34@cornell.edu>
2021-06-04 11:43:40 -0400

    Converting test_sub.py to pytest fixtures ARXIVNG-4182
    Encountered error in test with msg:
    
    Couldn't parse datetime string: '2015-11-17 15\\:07:23'
    
    The \\:0 is something about escaping the bind vars. So I just changed
    the data.sql to not have :0 in the times.

--- tests2/data.sql
+++ tests2/data.sql
@@ -46,7 +46,7 @@ VALUES
 INSERT INTO `arXiv_submissions`
 (`submission_id`, `document_id`, `doc_paper_id`, `sword_id`, `userinfo`, `is_author`, `agree_policy`, `viewed`, `stage`, `submitter_id`, `submitter_name`, `submitter_email`, `created`, `updated`, `status`, `sticky_status`, `must_process`, `submit_time`, `release_time`, `source_size`, `source_format`, `source_flags`, `has_pilot_data`, `is_withdrawn`, `title`, `authors`, `comments`, `proxy`, `report_num`, `msc_class`, `acm_class`, `journal_ref`, `doi`, `abstract`, `license`, `version`, `type`, `is_ok`, `admin_ok`, `allow_tex_produced`, `remote_addr`, `remote_host`, `package`, `rt_ticket_id`, `auto_hold`, `is_oversize`)
 VALUES
-(1137933,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-17 15:07:23','2015-11-17 15:11:21',1 /*status*/,NULL,0,NULL,NULL,500,'tex','1',0,0,'A very simple LaTeX example','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'A very simple LaTeX example.','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',0,0,0,'10.128.128.241','dhcp-gs-241.eduroam.cornell.edu','/users/e-prints/arXivLib/t/user_data/very_simple_1137933.gz',NULL,0,NULL);
+(1137933,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-17 15:37:23','2015-11-17 15:11:21',1 /*status*/,NULL,0,NULL,NULL,500,'tex','1',0,0,'A very simple LaTeX example','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'A very simple LaTeX example.','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',0,0,0,'10.128.128.241','dhcp-gs-241.eduroam.cornell.edu','/users/e-prints/arXivLib/t/user_data/very_simple_1137933.gz',NULL,0,NULL);
 
 
 INSERT INTO `arXiv_submission_category`
@@ -58,7 +58,7 @@ VALUES (1137933,'q-bio.NC',1,0);
 INSERT INTO `arXiv_submissions`
 (`submission_id`, `document_id`, `doc_paper_id`, `sword_id`, `userinfo`, `is_author`, `agree_policy`, `viewed`, `stage`, `submitter_id`, `submitter_name`, `submitter_email`, `created`, `updated`, `status`, `sticky_status`, `must_process`, `submit_time`, `release_time`, `source_size`, `source_format`, `source_flags`, `has_pilot_data`, `is_withdrawn`, `title`, `authors`, `comments`, `proxy`, `report_num`, `msc_class`, `acm_class`, `journal_ref`, `doi`, `abstract`, `license`, `version`, `type`, `is_ok`, `admin_ok`, `allow_tex_produced`, `remote_addr`, `remote_host`, `package`, `rt_ticket_id`, `auto_hold`, `is_oversize`, is_locked)
 VALUES
-(100430 ,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-17 15:07:23','2015-11-17 15:11:21',1 /*status*/,NULL,0,'2015-11-17 15:11:21',NULL,500,'tex','1',0,0,'A very simple LaTeX example','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'A very simple LaTeX example.','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',0,0,0,'10.128.128.241','dhcp-gs-241.eduroam.cornell.edu','/users/e-prints/arXivLib/t/user_data/very_simple_1137933.gz',NULL,0,NULL, 1);
+(100430 ,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-17 15:37:23','2015-11-17 15:11:21',1 /*status*/,NULL,0,'2015-11-17 15:11:21',NULL,500,'tex','1',0,0,'A very simple LaTeX example','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'A very simple LaTeX example.','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',0,0,0,'10.128.128.241','dhcp-gs-241.eduroam.cornell.edu','/users/e-prints/arXivLib/t/user_data/very_simple_1137933.gz',NULL,0,NULL, 1);
 
 
 INSERT INTO `arXiv_submission_category`
@@ -70,7 +70,7 @@ VALUES (100430,'q-bio.NC',1,0);
 INSERT INTO `arXiv_submissions`
 (`submission_id`, `document_id`, `doc_paper_id`, `sword_id`, `userinfo`, `is_author`, `agree_policy`, `viewed`, `stage`, `submitter_id`, `submitter_name`, `submitter_email`, `created`, `updated`, `status`, `sticky_status`, `must_process`, `submit_time`, `release_time`, `source_size`, `source_format`, `source_flags`, `has_pilot_data`, `is_withdrawn`, `title`, `authors`, `comments`, `proxy`, `report_num`, `msc_class`, `acm_class`, `journal_ref`, `doi`, `abstract`, `license`, `version`, `type`, `is_ok`, `admin_ok`, `allow_tex_produced`, `remote_addr`, `remote_host`, `package`, `rt_ticket_id`, `auto_hold`, `is_oversize`)
 VALUES
-(1137931,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-16 18:04:31','2015-11-17 13:37:25',1/*status*/,NULL,0,'2015-11-16 19:50:55',NULL,998628,'pdftex','',0,0,'First timeout fail case','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'First timeout fail case','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',NULL,NULL,0,'128.84.31.207','bbsurfacepro3.tc.cornell.edu','/users/e-prints/arXivLib/t/user_data/extraction_timeout1_1137931.tar.gz',NULL,1,NULL);
+(1137931,NULL,NULL,NULL,1,1,1,1,5,246231,'Brandon Barker','beb82@cornell.edu','2015-11-16 18:34:31','2015-11-17 13:37:25',1/*status*/,NULL,0,'2015-11-16 19:50:55',NULL,998628,'pdftex','',0,0,'First timeout fail case','Brandon Barker',NULL,NULL,NULL,NULL,NULL,NULL,NULL,'First timeout fail case','http://arxiv.org/licenses/nonexclusive-distrib/1.0/',1,'new',NULL,NULL,0,'128.84.31.207','bbsurfacepro3.tc.cornell.edu','/users/e-prints/arXivLib/t/user_data/extraction_timeout1_1137931.tar.gz',NULL,1,NULL);
 
 
 INSERT INTO `arXiv_submission_category`
---
+++ tests2/test_sub.py
@@ -0,0 +1,77 @@
+from modapi.auth import user_jwt
+
+SUB_ID_1 = 1137914
+
+
+def test_auth(client):
+    res = client.get(f"/submission/{SUB_ID_1}")
+    assert res.status_code == 401  # Should be auth protected
+
+    res = client.get("/submissions")
+    assert res.status_code == 401  # Should be auth protected
+
+
+def test_subs(client):
+    cookies = {
+        "ARXIVNG_SESSION_ID": user_jwt(246231)
+    }  # Brandon, mod of q-bio.CB and q-bio.NC
+    res = client.get("/submissions", cookies=cookies)
+    assert (
+        res.status_code != 401
+    )  # Tests must run with env var JWT_SECRET same as server
+    assert res.status_code == 200
+    assert res.json() is not None
+    sub = res.json()
+    assert type(sub) == list
+
+
+def test_sub(client):
+    cookies = {
+        "ARXIVNG_SESSION_ID": user_jwt(246231)
+    }  # Brandon, mod of q-bio.CB and q-bio.NC
+    res = client.get(f"/submission/{SUB_ID_1}", cookies=cookies)
+    assert res.status_code != 401  # Tests must run with env var JWT_SECRET same as server
+    assert res.status_code == 200
+    assert res.json() is not None
+    sub = res.json()
+
+    assert "status" in sub
+    assert type(sub["status"]) == str
+
+    assert sub["submitter"]["name"] == "Brandon Barker"
+
+    assert "classifier_scores" in sub["categories"]
+    assert sub["categories"]["classifier_scores"]
+    assert sub["comment_count"]
+
+    assert sub["categories"]["new_crosses"] == []
+    assert sub["categories"]["submission"]["secondary"] == []
+    assert sub["categories"]["submission"]["primary"] == 'q-bio.CB'
+
+
+def test_cross(client):
+    cookies = {
+        "ARXIVNG_SESSION_ID": user_jwt(246231)
+    }  # Brandon, mod of q-bio.CB and q-bio.NC
+    res = client.get(f"/submission/3400", cookies=cookies)
+    assert res.status_code != 401  # Tests must run with env var JWT_SECRET same as server
+    assert res.status_code == 200
+    assert res.json() is not None
+    sub = res.json()
+    assert sub["type"] == 'cross'
+    new_cross = 'hep-ph'
+    assert sub["categories"]["new_crosses"] == [new_cross]
+    assert new_cross not in sub["categories"]["submission"]["secondary"]
+    assert sub["categories"]["submission"]["secondary"] == ['cs.DD']
+
+
+def test_not_found(client):
+    cookies = {
+        "ARXIVNG_SESSION_ID": user_jwt(246231)
+    }  # Brandon, mod of q-bio.CB and q-bio.NC
+
+    res = client.get("/submission/0", cookies=cookies)
+    assert (
+        res.status_code != 200
+    )  # look for a missing SUB id in the test data (FYI, this exists in the real data)
+    assert res.status_code != 500
